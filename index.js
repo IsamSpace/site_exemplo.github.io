a1c267d435fe1843134c7896f6062e3d691fc97c
IsamSpace <155160846+IsamSpace@users.noreply.github.com>
2024-01-05 11:10:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function fecharMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
